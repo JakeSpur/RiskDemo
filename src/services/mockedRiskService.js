8e4cca470f5ee9037cd82f7f952c11cc22a12a71
Jake <jakeshelley@gmail.com>
2016-09-12 16:31:32 +1000

    updated mocked service api to serve dummy responses

--- src/services/mockedRiskService.js
+++ src/services/mockedRiskService.js
@@ -1,10 +1,21 @@
+const FAKE_SERVER_DELAY = 0;
+import settledBets from './settled.json';
+import unsettledBets from './unsettled.json';
+
+const wrapInSuccessfulPromise = (data) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(Object.assign([], data));
+        }, FAKE_SERVER_DELAY);
+    });
+};
+
 class RiskService {
-
     static getSettledBets() {
-
+        return wrapInSuccessfulPromise(settledBets);
     }
 
     static getUnSettledBets() {
-
+        return wrapInSuccessfulPromise(unsettledBets);
     }
 }
